2c8cafe6bc2f30d14a3e58d2c02d8a66b569bc94
thomasson4 <thomasolusegun02@gmail.com>
2022-06-14 14:58:37 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - function that prints the reverse of 
+ * string
+ * @s: character to be inputed
+ */
+
+void rev_string(char *s)
+{
+	int len = 0, max, half;
+	char first, last;
+	
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	max = len - 1;
+	half = max / 2;
+	while (half >= 0)
+	{
+		first = s[max - half];
+		last = s[half];
+		s[half] = first;
+		s[max - half] = last;
+		half--;
+	}
+}
+
