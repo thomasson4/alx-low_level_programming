599a5cdd060512dad4c775ce7c370823ce08caea
thomasson4 <thomasolusegun02@gmail.com>
2022-06-14 15:41:30 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - prints an array with size given
+ * @src: character to be inputed
+ * @dest: other character to be inputed
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len;
+
+	for (len = 0; *(src + len) != '\0'; len++)
+	{
+		dest[len] = *(src + len);
+	}
+	dest[len] = '\0';
+	return (dest);
+}
+
