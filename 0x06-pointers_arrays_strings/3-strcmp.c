ea982ba1d54d21c65f9d39edb0041deefe7343ea
thomasson4 <thomasolusegun02@gmail.com>
2022-06-16 16:07:12 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,7 +8,7 @@
  * Return: less than 0 if s1 is less than s2, 0 if they're equal,
  * more than 0 if s1 is greater than s2
  */
-int _strcmp(char *s1, char * s2)
+int _strcmp(char *s1, char *s2)
 {
 	while (*s1 == *s2)
 	{
