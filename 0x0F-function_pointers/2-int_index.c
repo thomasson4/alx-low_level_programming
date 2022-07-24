2423d38e45644a1b1499c51f574f3853c4ba4a3d
thomasson4 <thomasolusegun02@gmail.com>
2022-07-24 07:37:11 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -25,12 +25,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	}
 
-		for (i = 0; i < size; i++)
-		{
-			if ((*cmp)(array[i]) == 1)
-			{			
-				return (i);										
-			}
-		}
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]) == 1)
+		{			
+			return (i);								}
+		
+	}
 		return (-1);
 }
