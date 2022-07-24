f1dbc1a5227ef936cbeb6b98bb0daa24cca5d05a
thomasson4 <thomasolusegun02@gmail.com>
2022-07-24 13:13:33 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 /**
  * print_numbers - function that prints numbers, followed by a new line
- * 
+ *
  * @separator: is the string to be printed between numbers
  * @n: is the number of integers passed to the function
  */
