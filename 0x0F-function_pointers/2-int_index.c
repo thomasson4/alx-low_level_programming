3e476980f67dd403e88daf25cb350a18e077453f
thomasson4 <thomasolusegun02@gmail.com>
2022-07-24 08:21:13 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -29,8 +29,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		if ((*cmp)(array[i]) == 1)
 		{			
-			return (i);								}
-		
+			return (i);
+		}		
 	}
-		return (-1);
+	return (-1);
 }
