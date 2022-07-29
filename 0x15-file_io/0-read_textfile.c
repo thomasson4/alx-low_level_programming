5e0f91c62fa02a771f7d13b27f3aa6d1f2cd3092
thomasson4 <thomasolusegun02@gmail.com>
2022-07-29 10:57:22 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,25 +1,49 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-/* Libraries */
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
+/**
+ * read_textfile - reads a text file
+ * @filename: path of the file to read
+ * @letters: number of letters to read and print
+ * Return: number of letters readed.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	int count, fd_o, fd_r;
 
-/* MACROS */
-#define BUFFERSIZE 1024
-#define READ_ERROR 1
-#define WRITE_ERROR 2
+	if (filename == NULL)
+		return (0);
 
-/* prototypes of functions */
-ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
-char *create_buffer(char *fileName);
-int error_handler(int fd, char *fname, int type, char *buffer);
-void close_file(int fd);
+	/* Open file and get file descriptor */
+	fd_o = open(filename, O_RDONLY);
+	if (fd_o == -1)
+		return (0);
 
-#endif
+	/* allocate memory in buffer to read contents of fd */
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	/* read contents of fd */
+	fd_r = read(fd_o, buffer, letters);
+	if (fd_r == 0)
+	{
+		free(buffer);
+		close(fd_o);
+		return (0);
+	}
+
+	/* write to standard output */
+	count = write(STDOUT_FILENO, buffer, fd_r);
+	if (count == -1)
+	{
+		free(buffer);
+		close(fd_r);
+		close(fd_o);
+		return (0);
+	}
+	close(fd_r);
+	close(fd_o);
+
+	return (count);
+}
